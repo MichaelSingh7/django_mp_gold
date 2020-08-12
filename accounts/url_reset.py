02cad394d45dc3760df1a75ef1845d15c85b84ac
MichaelSingh7 <mikael.1@live.co.uk>
2020-08-12 19:53:51 +0000

    gitignore for files such as settings.py

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
         {'post_reset_confirm': reverse_lazy('password_reset_complete')},
         name='password_reset_confirm'),
-    url(r'^complete/$', password_reset_complete,
+    url('^complete/$', password_reset_complete,
         name='password_reset_complete')
 ]
