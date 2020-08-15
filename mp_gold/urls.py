b11b4ac9a433ae400b288a4893350ea9c17cae0b
MichaelSingh7 <mikael.1@live.co.uk>
2020-08-15 21:50:27 +0000

    Updated code so products page shows

--- mp_gold/urls.py
+++ mp_gold/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
-    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+
 
 ]
