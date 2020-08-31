8227b4f653b94bb264d10a59cdaaa5fd3e384519
MichaelSingh7 <mikael.1@live.co.uk>
2020-08-31 19:20:41 +0000

    updated settings.py

--- mp_gold/settings.py
+++ mp_gold/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://milestoneprojectgold.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['milestoneprojectgold.herokuapp.com', 'localhost']
 
 
 # Application definition
