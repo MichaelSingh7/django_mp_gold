d7c8525b6483b09224a09031f25103cfe438077d
MichaelSingh7 <mikael.1@live.co.uk>
2020-08-31 19:56:04 +0000

    updated settings.py

--- mp_gold/settings.py
+++ mp_gold/settings.py
@@ -162,7 +162,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_HOST = 's3-eu-west-2.amazonaws.com'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN = '{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
